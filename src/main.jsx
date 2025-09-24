98ab7367c27ecb15c1e856f44058abbe22219f93
joker-whysosireus <7kp2x@atomicmail.io>
2025-09-24 04:24:43 +0300

    ads

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
-import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import {TonConnectUIProvider} from "@tonconnect/ui-react"
-
-const manifestUrl = 'https://raw.githubusercontent.com/joker-whysosireus/manifest-info/main/manifest.txt';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
       <App />
-  </TonConnectUIProvider>
 );
